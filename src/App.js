3107ce0d9a2f90b74f705f93af8d3e7d2bfaa20f
Natalia-Pereyra <79956638+Natalia-Pereyra@users.noreply.github.com>
2021-06-03 15:46:28 +1200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import leaflogo from "./leaflogo.png";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={leaflogo}className="leaf-logo"></img>
+      The Green Dictionary
+      </header>
+    </div>
+  );
+}
+
+export default App;
