d771417e12ce38cbdcff1da463c65fea0c0bb8ac
Natalia-Pereyra <79956638+Natalia-Pereyra@users.noreply.github.com>
2021-06-04 16:37:33 +1200

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import leaflogo from "./leaflogo.png";
+import React from "react";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        <img src={leaflogo} className="leaf-logo" alt="logo"></img>
-      The Green Dictionary
+      The <span>Green</span> Dictionary
+      <h2>
+        For every 100 words searched a tree is being planted
+      </h2>
       </header>
+      <Dictionary />
+      <footer><small>coded by me</small></footer>
+      </div>
     </div>
   );
 }
